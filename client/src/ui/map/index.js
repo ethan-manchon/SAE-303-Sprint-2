43872531f68e83cc4da53a5b78d41153d081f28b
Ethan Manchon <manchon.ethan@gmail.com>
2024-12-16 15:21:36 +0100

    Add files via upload

--- client/src/ui/map/index.js
+++ client/src/ui/map/index.js
@@ -1,6 +1,7 @@
 let MapView = {};
 
-MapView.render = function(lycees) {
+MapView.render = function(lycees){
+
     var map = L.map('map').setView([45.85, 1.25], 9);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 12,
@@ -8,17 +9,15 @@ MapView.render = function(lycees) {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    function RenderLycees(lycees) {
-
+    function RenderLycees() {
         lycees.forEach(lycee => {
             if (lycee.latitude && lycee.longitude && !isNaN(lycee.latitude) && !isNaN(lycee.longitude)) {
                 var marker = L.marker([lycee.latitude, lycee.longitude]).addTo(map);
-                console.log(lycee);
                 marker.bindPopup("<b>"+lycee.denomination_principale + "</b><br>"+lycee.libelle_commune).openPopup();
             }
         });
-    }
-    RenderLycees(lycees);
-};
-
-export { MapView };
+    };
+    
+    RenderLycees();
+}
+export {MapView};
